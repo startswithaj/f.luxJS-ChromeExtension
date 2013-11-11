dd39be4a92861fa0b6bc2bb1d86a85e28336edd7
Jacob McCutcheon <jacob.mccutcheon@gmail.com>
2013-11-12 01:50:03 +1000

    Initial Commit

---
+++ flux.js
@@ -0,0 +1,30 @@
+(function() 
+{
+	var body = document.getElementsByTagName('body')[0];
+	var modal = document.createElement('div');
+	//modal.style.background = "rgba(100%, 100%, 50%, 0.3)";
+	modal.style.position = 'fixed';
+	modal.style.top	= 0;
+	modal.style.bottom = 0;
+	modal.style.left = 0;
+	modal.style.right = 0;
+	modal.style.pointerEvents = "none"
+
+	modal.style.zIndex = 999999999999999999
+	modal.style.visibility = "hidden"
+
+	body.appendChild(modal)
+
+	now = new Date().getTime();
+	if ( ((now < window.sunset) && (now < window.sunrise)) || ((now > window.sunrise) && (now > window.sunset)) ) {
+		setTimeout(function(){
+			modal.style.visibility = "visible" // finally a use
+			modal.style.background = "rgba(100%, 100%, 50%, 0.3)";
+			modal.style.transition = "background 3s" //ease 1s";
+		}, 10);
+	}
+
+})();
+
+
+
