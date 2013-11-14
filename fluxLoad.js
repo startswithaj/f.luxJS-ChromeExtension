516afadae9b6047b283533de30aeb77f6ff7fd71
Jacob McCutcheon <jacob.mccutcheon@gmail.com>
2013-11-14 19:26:05 +1000

    all in fluxLoad.js

--- fluxLoad.js
+++ fluxLoad.js
@@ -1,4 +1,5 @@
-var isOn = false
+this.isOn = false
+this.modal = null
 
 chrome.runtime.onMessage.addListener (
     function (request, sender, sendResponse) {
@@ -6,9 +7,9 @@ chrome.runtime.onMessage.addListener (
         if (request.command == "toggle") {
             isOn = !isOn;
             if (isOn)
-                document.getElementById('fluxColor').style.display = 'block';
+                this.modal.style.background = "rgba(100%, 80%, 50%, 0.2)";
             else
-                document.getElementById('fluxColor').style.display = 'none';
+                this.modal.style.background = "none";
             sendResponse({isOn: isOn})
         }
     }
@@ -26,15 +27,71 @@ chrome.runtime.sendMessage ( {command: "getLocation"}, function (response) {
     console.log(times);
     console.log(sunset);
     if ( ((now < sunset) && (now < sunrise)) || ((now > sunrise) && (now > sunset)) ) {
-    	var s = document.createElement('script');
-    	s.src = chrome.extension.getURL("flux.js");
-    	s.onload = function() {
-    	    this.parentNode.removeChild(this);
-    	};
-    	(document.head||document.documentElement).appendChild(s);
-        chrome.runtime.sendMessage( {command: "setBadge"} );
+    	// var s = document.createElement('script');
+    	// s.src = chrome.extension.getURL("flux.js");
+    	// s.onload = function() {
+    	//     this.parentNode.removeChild(this);
+    	// };
+    	// (document.head||document.documentElement).appendChild(s);
+        var body = document.getElementsByTagName('body')[0];
+        modal = document.createElement('div');
+        //modal.style.background = "rgba(100%, 100%, 50%, 0.3)";
+        modal.id = "fluxColor"
+        modal.style.position = 'fixed';
+        modal.style.top = 0;
+        modal.style.bottom = 0;
+        modal.style.left = 0;
+        modal.style.right = 0;
+        modal.style.pointerEvents = "none";
+        modal.style.zIndex = 999999999999999999;
+
+        // var button = document.createElement('div');
+        // button.innerHTML = "on"
+        // button.style.position = 'fixed';
+        // button.style.right = 0;
+        // button.style.top = 0;
+        // button.style.width = '32px';
+        // button.style.height = '32px';
+        // button.style.backgroundImage = "url('http://justgetflux.com/favicon.png')";
+        // button.style.backgroundRepeat = 'no-repeat';
+        // button.style.font = '12px Helvetica';
+        // button.style.textAlign = 'center';
+        // button.style.verticalAlign = 'middle';
+        // button.style.lineHeight = '32px';
+        // button.style.color = 'darkslategray';
+        // button.style.fontWeight = 'bold';
+        // button.style.textShadow = '0px 1px 0px #e5e5ee';
+        // button.style.cursor = 'pointer';
+        // button.style.webkitUserSelect = 'none'; 
+        // button.style.zIndex = 999999999999999999;
+
+
+        body.appendChild(modal)
+        // body.appendChild(button)
         isOn = true
-    }
+        // button.onclick = (function(button, modal, isOn) {
+        //     return function(){
+        //         if (isOn) {
+        //             modal.style.background = "none";
+        //             button.innerHTML = 'off'
+        //             isOn = false
+        //         } else {
+        //             modal.style.background = "rgba(100%, 80%, 50%, 0.2)";
+        //             button.innerHTML = 'on'
+        //             isOn = true
+        //         }
+
+        //     };
+        // })(button, modal, isOn);
+
+        setTimeout(function(){
+            modal.style.visibility = "visible" // finally a use
+            modal.style.background = "rgba(100%, 80%, 50%, 0.2)";
+            modal.style.transition = "background 5s" //ease 1s";
+        }, 1);
+            chrome.runtime.sendMessage( {command: "setBadge"} );
+            isOn = true
+        }
 
 });
 
